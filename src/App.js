5c30f9319a561f4b245736378779fc9a32541a90
nosinxever <nosinxever@gmail.com>
2022-08-07 23:12:48 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import AllinOne from "./components/AllinOne";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Narbar from "./components/Narbar";
+import Priceing from "./components/Priceing";
+import Support from "./components/Support";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Narbar/>
+    <Hero/>
+    <About/>
+    <Support/>
+    <AllinOne/>
+    <Priceing/>
+    <Footer/>
+    </>
   );
 }
 
